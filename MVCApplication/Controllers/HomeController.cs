9aafd5556a2734b747cb10d032c9a97c1b4fc8ed
mystified131 <mystifiedthomas@gmail.com>
2019-01-06 07:43:05 -0600

    Initial commit.

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using MVCApplication.ViewModels;
+
+namespace MVCApplication.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public static List<Shape> TheList = new List<Shape>();
+
+        public IActionResult Index()
+        {
+            IndexViewModel indexViewModel = new IndexViewModel();
+
+            return View(indexViewModel);
+        }
+
+        public IActionResult Error()
+        {
+
+            return View();
+        }
+        
+        public IActionResult AllData()
+        {
+            AllDataViewModel allDataViewModel = new AllDataViewModel();
+
+            List<Dictionary<string, string>> elements = CSVData.FindAll();
+
+            allDataViewModel.elements = elements;
+
+            return View(allDataViewModel);
+        }
+
+    }
+
+
+
+}
---
+++ MVCApplication/ViewModels/AllDataViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.ViewModels
+{
+    public class AllDataViewModel
+    {
+        public List<Dictionary<string, string>> elements { get; set; }
+    }
+}
